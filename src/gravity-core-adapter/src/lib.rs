37194b78370d8b0c63f68236a5d55699e7b85695
Shamil Khalilov <shamfama@gmail.com>
2021-04-19 21:43:21 +0300

    Add internal ser/deser tests for gravity contract

--- src/gravity-core-adapter/src/lib.rs
+++ src/gravity-core-adapter/src/lib.rs
@@ -4,6 +4,7 @@ mod error;
 
 mod gravity;
 mod state;
+mod tests;
 
 #[cfg(not(feature = "no-entrypoint"))]
 pub mod entrypoint;
--- src/gravity-core-adapter/src/state.rs
+++ src/gravity-core-adapter/src/state.rs
@@ -3,10 +3,12 @@ use solana_program::{
     program_pack::{IsInitialized, Pack, Sealed},
     pubkey::Pubkey,
 };
+use rand::random;
 
 use arrayref::{array_mut_ref, array_ref, array_refs, mut_array_refs};
 
 
+#[derive(PartialEq, PartialOrd, Default, Clone)]
 pub struct GravityContract {
     pub is_initialized: bool,
     pub initializer_pubkey: Pubkey,
@@ -15,6 +17,7 @@ pub struct GravityContract {
     pub consuls: Vec<Pubkey>,
     pub last_round: u64
 }
+
 
 impl Sealed for GravityContract {}
 
@@ -25,7 +28,8 @@ impl IsInitialized for GravityContract {
 }
 
 impl Pack for GravityContract {
-    const LEN: usize = 202;
+    const LEN: usize = 138;
+
     fn unpack_from_slice(src: &[u8]) -> Result<Self, ProgramError> {
         let src = array_ref![src, 0, GravityContract::LEN];
         let (
@@ -34,7 +38,7 @@ impl Pack for GravityContract {
             bft,
             consuls,
             last_round,
-        ) = array_refs![src, 1, 32, 1, 32 * 5, 8];
+        ) = array_refs![src, 1, 32, 1, 32 * 3, 8];
         let is_initialized = match is_initialized {
             [0] => false,
             [1] => true,
@@ -62,7 +66,7 @@ impl Pack for GravityContract {
             bft_dst,
             consuls_dst,
             last_round_dst,
-        ) = mut_array_refs![dst, 1, 32, 1, 32 * 5, 8];
+        ) = mut_array_refs![dst, 1, 32, 1, 32 * 3, 8];
 
         let GravityContract {
             is_initialized,
@@ -88,74 +92,42 @@ impl Pack for GravityContract {
     }
 }
 
+#[cfg(test)]
+mod tests {
+    use std::error;
 
-// pub struct Escrow {
-//     pub is_initialized: bool,
-//     pub initializer_pubkey: Pubkey,
-//     pub temp_token_account_pubkey: Pubkey,
-//     pub initializer_token_to_receive_account_pubkey: Pubkey,
-//     pub expected_amount: u64,
-// }
+    use super::*;
 
-// impl Sealed for Escrow {}
+    type WrappedResult<T> = Result<T, Box<dyn error::Error>>;
 
-// impl IsInitialized for Escrow {
-//     fn is_initialized(&self) -> bool {
-//         self.is_initialized
-//     }
-// }
+    #[test]
+    fn test_ser_deser() -> WrappedResult<()> {
+        let mock_gravity_consuls = vec![
+            Pubkey::new_unique(),
+            Pubkey::new_unique(),
+            Pubkey::new_unique(),
+        ];
+        let mock_bft: u8 = random();
+        let mock_last_round: u64 = random();
 
-// impl Pack for Escrow {
-//     const LEN: usize = 105;
-//     fn unpack_from_slice(src: &[u8]) -> Result<Self, ProgramError> {
-//         let src = array_ref![src, 0, Escrow::LEN];
-//         let (
-//             is_initialized,
-//             initializer_pubkey,
-//             temp_token_account_pubkey,
-//             initializer_token_to_receive_account_pubkey,
-//             expected_amount,
-//         ) = array_refs![src, 1, 32, 32, 32, 8];
-//         let is_initialized = match is_initialized {
-//             [0] => false,
-//             [1] => true,
-//             _ => return Err(ProgramError::InvalidAccountData),
-//         };
+        let gravity_contract_mock = GravityContract {
+            consuls: mock_gravity_consuls.clone(),
+            bft: mock_bft,
+            last_round: mock_last_round,
+            ..GravityContract::default()
+        };
 
-//         Ok(Escrow {
-//             is_initialized,
-//             initializer_pubkey: Pubkey::new_from_array(*initializer_pubkey),
-//             temp_token_account_pubkey: Pubkey::new_from_array(*temp_token_account_pubkey),
-//             initializer_token_to_receive_account_pubkey: Pubkey::new_from_array(
-//                 *initializer_token_to_receive_account_pubkey,
-//             ),
-//             expected_amount: u64::from_le_bytes(*expected_amount),
-//         })
-//     }
+        // serialize
+        // let mut serialized_gravity_contract: Vec<u8> = Vec::new();
+        let mut serialized_gravity_contract_bytes = [0 as u8; GravityContract::LEN];
+        gravity_contract_mock.pack_into_slice(&mut serialized_gravity_contract_bytes);
 
-//     fn pack_into_slice(&self, dst: &mut [u8]) {
-//         let dst = array_mut_ref![dst, 0, Escrow::LEN];
-//         let (
-//             is_initialized_dst,
-//             initializer_pubkey_dst,
-//             temp_token_account_pubkey_dst,
-//             initializer_token_to_receive_account_pubkey_dst,
-//             expected_amount_dst,
-//         ) = mut_array_refs![dst, 1, 32, 32, 32, 8];
+        // deserialize
+        let deserialized_gravity_contract = GravityContract::unpack_from_slice(&mut serialized_gravity_contract_bytes)
+            .expect("deserialization failed!");
 
-//         let Escrow {
-//             is_initialized,
-//             initializer_pubkey,
-//             temp_token_account_pubkey,
-//             initializer_token_to_receive_account_pubkey,
-//             expected_amount,
-//         } = self;
+        assert!(deserialized_gravity_contract == gravity_contract_mock);
 
-//         is_initialized_dst[0] = *is_initialized as u8;
-//         initializer_pubkey_dst.copy_from_slice(initializer_pubkey.as_ref());
-//         temp_token_account_pubkey_dst.copy_from_slice(temp_token_account_pubkey.as_ref());
-//         initializer_token_to_receive_account_pubkey_dst
-//             .copy_from_slice(initializer_token_to_receive_account_pubkey.as_ref());
-//         *expected_amount_dst = expected_amount.to_le_bytes();
-//     }
-// }
+        Ok(())
+    }
+}
