833ab9a7fbe5e9c8d8d20dceacddb1733f5eb1d8
Shamil Khalilov <shamfama@gmail.com>
2021-05-22 19:46:26 +0300

    Fix gravity contract bft

--- src/gravity-core-adapter/gravity/src/gravity/instruction.rs
+++ src/gravity-core-adapter/gravity/src/gravity/instruction.rs
@@ -18,7 +18,6 @@ use crate::gravity::state::GravityContract;
 use crate::gravity::error::GravityError::InvalidInstruction;
 use crate::gravity::allocs::allocation_by_instruction_index;
 
-
 pub enum GravityContractInstruction {
     InitContract {
         new_consuls: Vec<Pubkey>,
@@ -31,15 +30,12 @@ pub enum GravityContractInstruction {
     },
 }
 
-impl<'a> GravityContractInstruction {
+impl GravityContractInstruction {
     pub const BFT_ALLOC: usize = 1;
     pub const PUBKEY_ALLOC: usize = 32;
     pub const LAST_ROUND_ALLOC: usize = 8;
 
-    pub const BFT_RANGE: std::ops::Range<usize> = 0..Self::BFT_ALLOC;
-
-    /// Unpacks a byte buffer into a [EscrowInstruction](enum.EscrowInstruction.html).
-    pub fn unpack(input: &'a [u8]) -> Result<Self, ProgramError> {
+    pub fn unpack(input: &[u8]) -> Result<Self, ProgramError> {
         let (tag, rest) = input.split_first().ok_or(InvalidInstruction)?;
 
         Ok(match tag {
@@ -54,6 +50,7 @@ impl<'a> GravityContractInstruction {
                 let current_round = extract_from_range(rest, ranges[1].clone(), |x: &[u8]| {
                     u64::from_le_bytes(*array_ref![x, 0, 8])
                 })?;
+
                 let initial_consuls = retrieve_consuls(rest, ranges[2].clone(), bft)?;
 
                 Self::InitContract {
@@ -84,46 +81,4 @@ impl<'a> GravityContractInstruction {
         })
     }
 
-    fn unpack_bft(input: &'a [u8]) -> Result<u8, ProgramError> {
-        Ok(input
-            .get(Self::BFT_RANGE)
-            .and_then(|slice| slice.try_into().ok())
-            .map(u8::from_le_bytes)
-            .ok_or(InvalidInstruction)?)
-    }
-
-    fn unpack_consuls(input: &'a [u8], dst: &mut Vec<Pubkey>) -> Result<(), ProgramError> {
-        let bft: u8 = Self::unpack_bft(input)?;
-
-        let range_start = Self::BFT_ALLOC;
-        let range_end = range_start + 32 * bft as usize;
-        let consuls_slice = input
-            .get(range_start..range_end)
-            .ok_or(InvalidInstruction)?;
-
-        // assert!(consuls_slice.len() == 3 * 32);
-
-        let address_alloc: usize = 32;
-
-        for i in 0..bft as usize {
-            let pubky = Pubkey::new(
-                consuls_slice
-                    .get(i * address_alloc..(i + 1) * address_alloc)
-                    .ok_or(InvalidInstruction)?,
-            );
-            dst.push(pubky);
-        }
-
-        Ok(())
-    }
-
-    /// Round is considered as first argument and as u256 data type
-    fn unpack_round(bft: u8, input: &[u8]) -> Result<u64, ProgramError> {
-        let start_offset = Self::BFT_ALLOC + (Self::PUBKEY_ALLOC * bft as usize);
-        Ok(input
-            .get(start_offset..start_offset + Self::LAST_ROUND_ALLOC)
-            .and_then(|slice| slice.try_into().ok())
-            .map(u64::from_le_bytes)
-            .ok_or(InvalidInstruction)?)
-    }
 }
