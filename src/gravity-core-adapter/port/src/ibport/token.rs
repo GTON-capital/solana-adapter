542fbaa34b7d2b5cf2d63f251eb3ea4f62537c27
Shamil Khalilov <shamfama@gmail.com>
2021-07-10 14:11:42 +0300

    Clean

--- src/gravity-core-adapter/port/src/ibport/token.rs
+++ src/gravity-core-adapter/port/src/ibport/token.rs
@@ -3,11 +3,6 @@ use solana_program::{declare_id, pubkey::Pubkey};
 
 // declare_id!("nVZnRKdr3pmcgnJvYDE8iafgiMiBqxiffQMcyv5ETdA");
 
-// let my_id = Pubkey::from_str("nVZnRKdr3pmcgnJvYDE8iafgiMiBqxiffQMcyv5ETdA").unwrap();
-// assert_eq!(id(), my_id);
-
-
-// declare_id()
 pub fn susy_wrapped_gton_mint() -> Pubkey {
     Pubkey::from_str("nVZnRKdr3pmcgnJvYDE8iafgiMiBqxiffQMcyv5ETdA").unwrap()
 }
